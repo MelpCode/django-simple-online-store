1db9298fb307754f4ead7c51311e23389fb71f1e
MelpCode <67125923+MelpCode@users.noreply.github.com>
2021-03-13 16:36:33 -0500

    Update settings.py

--- django_melpstore/settings.py
+++ django_melpstore/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!$zgpbu^^d$aak=y8p_^xlke@rrc+q31qpf)-ekl7yyt2tgi+2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
